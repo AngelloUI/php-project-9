7306e84fbb9ba3df1979841f2df4171036182b6a
AngelloUI <usefullforit@gmail.com>
2025-06-13 23:14:06 +0300

    add main page

--- public/index.php
+++ public/index.php
@@ -5,13 +5,27 @@ declare(strict_types=1);
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 $app = AppFactory::create();
+
+$twig = Twig::create(__DIR__ . '/../templates', [
+    'cache' => false,
+    'debug' => true]
+);
+$app->add(TwigMiddleware::create($app, $twig));
+
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
+$app->get('/welcome', function ($request, $response) use ($twig) {
 
-    return $response;
-});
+    return $twig->render($response, 'welcome.html.twig');
+})->setName('welcome');
+
+$app->get('/', function ($request, $response) use ($twig) {
+
+    return $twig->render($response, 'main.html.twig');
+})->setName('main');
+
 $app->run();
